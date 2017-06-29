dfaf7cff3a522450975d50d1bb2dcb4fa9ffcfee
Kamil Kokot <kamil@kokot.me>
2017-06-29 17:30:53 +0200

    Fix Crowdin URL

---
+++ src/Crowdin/Client.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SyliusBot\Crowdin;
+
+use Crowdin\Client as BaseClient;
+
+final class Client extends BaseClient
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getHttpClient()
+    {
+        $httpClient = parent::getHttpClient();
+
+        // Hello abandoned packages, here we meet again!
+        $httpClient->setBaseUrl('http://api.crowdin.com/api');
+
+        return $httpClient;
+    }
+}
